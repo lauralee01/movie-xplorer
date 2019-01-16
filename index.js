7a0c71d8ce10c1e07974bf18e515bbb1837b6a62
lauralee01 <lauraoghwono@gmail.com>
2019-01-16 15:43:28 +0100

    consuming movie api

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ function append(parent, el) {
 	return parent.appendChild(el);
 }
 
-fetch("https://api.themoviedb.org/3/tv/popular?api_key=a4d9ad8d2d072c50dc998cc0d1a508fa&language=en-US&page=1")
+const URL = "https://api.themoviedb.org/3/tv/popular?api_key=a4d9ad8d2d072c50dc998cc0d1a508fa&language=en-US&page=1"
+
+fetch(URL)
 	.then((response) => response.json())
 	.then((data) => {
 		let tvshows = data.results;
 		console.log(tvshows);
 		return tvshows.map((tvshow) => {
 			let li = createNode('li'),
-			a = createNode('a');
-			a.innerHTML = tvshow.original_name;
-			append(li, a);
+				img = createNode('img'),	
+				span = createNode('span');
+				img.src = `https://image.tmdb.org/t/p/w185/${tvshow.poster_path}`;
+				console.log(img.src)
+			span.innerHTML = `<br>Name - ${tvshow.original_name}<br> Overview - ${tvshow.overview}<br> First Air-Date - ${tvshow.first_air_date}<br> Vote Average ${tvshow.vote_average}`;
+			append(li, img);
+			append(li, span);
 			append(ul, li);
 		})
 
 	})
+
